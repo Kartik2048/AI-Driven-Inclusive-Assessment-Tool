d1f6daacb553b26887c9b0b234ebd33d1d7e0131
Kartik2048 <142119629+Kartik2048@users.noreply.github.com>
2026-02-11 10:31:43 +0530

    Fix formatting issues in mongodb.py

--- config/mongodb.py
+++ config/mongodb.py
@@ -1,8 +1,9 @@
 from pymongo import MongoClient
 
 MONGO_URI = "<MONGODB URI>" //enter your uri
-client = MongoClient('<MONGODB CLIENT PERSONAL CODE>') //enter your code
+client = MongoClient('<MONGODB CLIENT PERSONAL CODE>') #enter your code
 db = client["assessment_db"]
 questions_collection = db["writing_questions"]
 
 users_collection = db["users"]
+
